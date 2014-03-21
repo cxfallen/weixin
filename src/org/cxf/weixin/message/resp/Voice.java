45d1d2a94ee5cc66908fd5740987c1430c7c8430
cxfallen <cxfallen921@gmail.com>
2014-03-21 17:18:10 +0800

    a

---
+++ src/org/cxf/weixin/message/resp/Voice.java
@@ -0,0 +1,14 @@
+package org.cxf.weixin.message.resp;
+
+public class Voice {
+	public String getMediaId() {
+		return MediaId;
+	}
+
+	public void setMediaId(String mediaId) {
+		MediaId = mediaId;
+	}
+
+	private String MediaId;
+
+}
