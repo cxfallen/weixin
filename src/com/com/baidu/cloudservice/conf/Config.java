241f5a67b043afb50b84a15e884ffbf55ead6c58
cxfallen <cxfallen921@gmail.com>
2014-03-24 11:19:59 +0800

    A

---
+++ src/com/com/baidu/cloudservice/conf/Config.java
@@ -0,0 +1,19 @@
+package com.baidu.cloudservice.conf;
+
+public class Config{
+
+  	/*** API KEY AND ACCESS KEY ***/
+	public static String USER = "XKGWiGQiWaZOsiHPWCHTnrsw";
+	public static String PWD = "qYIyXC1TlAFODhMBGkWFYE67tj23kd8v";
+
+  	/***MySQL相关配置***/
+	public static String MYSQLNAME = "eiZrxDYagZRYcJuTRljT";
+	public static String MYSQLHOST = "sqld.duapp.com";
+	public static String MYSQLPORT = "4050";
+
+	/***BCS配置***/
+  	public static String BCSHOST="bcs.duapp.com";
+  	public static String BUCKET="xxxx";
+  
+
+}
